1322987db8462e74574c317968ac0379ab06ff13
JrSchild <joriami@gmail.com>
2015-10-01 10:39:44 +0200

    Dummy Statistics class

---
+++ core/Statistics.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var EventEmitter = require('events');
+
+/**
+ * For now this class simply intercepts statistics and allows other classes
+ * to listen for events or something like this. It's still a little abstract
+ * but it will defintely be necessary.
+ */
+class Statistics extends EventEmitter {
+  constructor(threads) {
+    this.threads = threads;
+    this.latency = [];
+    this.operationsPerSecond = [];
+  }
+}
+
+module.exports = Statistics;
