a975e7a65d4859202daf064feb7aea9beaf8b9d9
JrSchild <joriami@gmail.com>
2015-10-01 10:39:34 +0200

    Implement PIDController to better approach the operations per second in ThroughputController.

--- core/ThroughputController.js
+++ core/ThroughputController.js
@@ -1,7 +1,9 @@
 "use strict"
 
 var _ = require('lodash');
+var PIDController = require('./PIDController.js');
 var settings = require('../settings.json').throughput;
+var getOpsPerSecInRange;
 
 /**
  * This class is used to control the number of operations per second
@@ -14,11 +16,11 @@ class ThroughputController {
     this.reset();
     this.initThreads();
     this.initThroughputEmitter();
+    this.currentLatency = 0;
   }
 
   reset() {
-    this.operationsPerSecond = settings.startOperationsPerSecond;
-    this.deltaOperationsPerSecond = settings.startDeltaOperationsPerSecond;
+    this.operationsPerSecond = settings.minOperationsPerSecond;
     this.latencies = [];
   }
 
@@ -33,37 +35,37 @@ class ThroughputController {
     }
   }
 
-  // Collect latencies for roughly 1 second and do its magic.
+  /**
+   * Collect latencies for 1000ms and adjust operationsPerSecond
+   * with a PID Controller to approach targetLatency.
+   */
   initThroughputEmitter() {
+    this.pid = new PIDController(4, 0.01, 0.1);
+    this.pid.setTarget(settings.targetLatency);
+
     setInterval(() => {
-      var currentLatency = _.sum(this.latencies) / this.latencies.length;
+      var currentLatency, correction;
+
+      currentLatency = _.sum(this.latencies) / this.latencies.length;
 
       // Start fresh on next iteration.
       this.latencies = [];
 
-      // If average is 0 or NaN (in case length is 0) the threads havent started yet.
+      // If average is 0 or NaN (in case length is 0) the threads haven't started yet.
       if (!currentLatency) {
         return;
       }
 
-      // When currentLatency is under the targetLatency multiply by 4 to quickly increase latency.
-      // Otherwise slowly bring the latency back down by dividing by 2.
-      // A better algorithm would be to use the PIDController. (npm: node-pid-controller)
-      if (currentLatency < settings.targetLatency) {
-        this.deltaOperationsPerSecond = Math.abs(this.deltaOperationsPerSecond) << 2;
-      } else {
-        this.deltaOperationsPerSecond = -(Math.abs(this.deltaOperationsPerSecond) >> 1);
-      }
+      // Use PID algorithm to calculate correction.
+      correction = this.pid.update(currentLatency);
 
-      // If delta is 0, reset to 1, otherwise there is nothing to bitshift.
-      this.deltaOperationsPerSecond = this.deltaOperationsPerSecond || 1;
-
-      // Update operationsPerSecond and send out data.
-      this.operationsPerSecond += this.deltaOperationsPerSecond;
+      // Update operationsPerSecond, make sure its between min and max value and send out data.
+      this.operationsPerSecond = getOpsPerSecInRange(this.operationsPerSecond + correction);
+      this.currentLatency = currentLatency;
       this.emitOperationsPerSecond();
 
-      // console.log(`current avg latency ${currentLatency}, new opsPerSec: ${this.operationsPerSecond}, deltaOps: ${this.deltaOperationsPerSecond}`);
-    }, settings.setInterval);
+      // console.log(`latency: ${currentLatency}, correction: ${correction > 0 ? '+' : ''}${correction}, opsPSec: ${this.operationsPerSecond}`);
+    }, settings.PIDInterval);
   }
 
   // Measure average latency for the last second.
@@ -78,5 +80,17 @@ class ThroughputController {
     }));
   }
 }
+
+/**
+ * Get the operations per seconds but makes sure they are
+ * in the range of min and max operations per second.
+ * Caches the min, max values.
+ */
+getOpsPerSecInRange = () => {
+  var min = settings.minOperationsPerSecond;
+  var max = settings.maxOperationsPerSecond;
+
+  return (value) => Math.min(max, Math.max(min, value));
+}();
 
 module.exports = ThroughputController;
