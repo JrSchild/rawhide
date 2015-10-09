bba69b579092d54570d8142dd84465fad99984dd
JrSchild <joriami@gmail.com>
2015-10-09 14:43:33 +0200

    Experimental LimitQueue class. Doesn't do much right now.

---
+++ core/LimitQueue.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var _ = require('lodash');
+
+/**
+ * A limited queue utility class that that can calculate the average.
+ */
+class LimitQueue {
+  construct(limit) {
+    this.limit = limit;
+    this.data = Array(limit);
+  }
+
+  push(elem) {
+    this.data.push(elem);
+    this.data.shift();
+  }
+
+  average() {
+    return _.sum(this.latencies) / this.limit;
+  }
+}
+
+module.exports = LimitQueue;
