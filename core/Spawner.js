2272333960cfbfa7135af90c03d3baff365dce6e
JrSchild <joriami@gmail.com>
2015-10-15 16:12:36 +0200

    Further improve error handling.

--- core/Spawner.js
+++ core/Spawner.js
@@ -21,7 +21,7 @@ class Spawner {
 
     this.started = true;
 
-    Promise.all(this.threadsConnected).then(() => console.log('All threads are connected.')).catch(console.error);
+    Promise.all(this.threadsConnected).then(() => console.log('All threads are connected.'));
     Promise.all(this.threadsLoaded).then(() => console.log('All threads are loaded.'));
 
     Promise.all(this.threadsConnected).then(() => this.sendToThreads(!this.settings.skipLoadingPhase ? 'load' : 'run'));
@@ -54,10 +54,12 @@ class Spawner {
       } else if (message.type === 'finishedLoading') {
         resolverLoaded.resolve();
       } else if (message.type === 'errorConnecting') {
-        resolverConnected.reject(JSON.stringify(message.data));
-        resolverLoaded.reject();
+        resolverConnected.reject(message.data);
+        // resolverLoaded.reject();
       }
     });
+
+    resolverConnected.promise.catch(console.error);
 
     // Initialize the client with settings and add to list of threads.
     this.sendToProcess('init', {
