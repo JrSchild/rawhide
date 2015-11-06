44feb1472357025cbb4288203f2f593a93a83dd3
JrSchild <joram.ruitenschild@gmail.com>
2015-11-06 11:01:36 +0100

    Simplify Workload, remove old code.

--- core/Workload.js
+++ core/Workload.js
@@ -18,9 +18,8 @@ class Workload {
 
     this.model = new (loader(`./models/${parameters.thread.model}`))(parameters);
     this.model.connect()
-      .then(() => this.run());
+      .then(() => this.start());
     this.discreteGenerator = new DiscreteGenerator(parameters.thread.proportions);
-    this.operationMethod = () => this[this.discreteGenerator.next](this.counter.add());
 
     process.on('message', (message) => {
       if (message.type === 'setOperationsPerSecond') {
@@ -31,8 +30,12 @@ class Workload {
     });
   }
 
-  run() {
-    this.execute('Running', this.parameters.operations, this.operationMethod);
+  start() {
+    this.initQueueCountInterval();
+  }
+
+  operationMethod() {
+    return this[this.discreteGenerator.next](this.counter.add());
   }
 
   // Immediately execute a set amount of operations.
@@ -50,50 +53,18 @@ class Workload {
     }
   }
 
-  execute(name, operations, operationMethod) {
-    if (this[`is${name}`] || this.isBusy) return;
-    // console.log(`Start ${name}.`);
+  initQueueCountInterval() {
+    this.queueCountInterval = setInterval(() => {
+      if (!this.counter) {
+        return;
+      }
 
-    this.isBusy = true;
-    this[`is${name}`] = true;
-    this[`start${name}Time`] = Date.now();
-    this.counter = new LimitCounter(operations, (err) => {
-      if (err) throw err;
-
-      this[`is${name}`] = false;
-      this.isBusy = false;
-
-      var end = Date.now() - this[`start${name}Time`];
-      console.log(`finished ${name}`, ~~(end / 1000) + 's', `(${end})ms`);
-      process.send({
-        type: `finished${name}`
-      });
-    });
-    this.emitCounterInterval = setInterval(() => {
       process.send({
         type: 'queueCount',
         pid: process.pid,
         data: this.counter.prepCurrent - this.counter.current
       });
     }, 500);
-    this.loadingInterval = setInterval(this.runSecond.bind(this, operationMethod), 1000);
-  }
-
-  runSecond(operationMethod) {
-    var interval = 1000 / settings.loadSplit;
-    var opsPerInterval = ~~(this.operationsPerSecond / settings.loadSplit);
-
-    if (this.counter.isLimit) {
-      clearInterval(this.emitCounterInterval);
-      return clearInterval(this.loadingInterval);
-    }
-
-    // Divide the operations per second over `settings.loadSplit` number of intervals.
-    for (let i = 0; i < settings.loadSplit; i++) {
-      setTimeout(() => {
-        for (let l = opsPerInterval; l-- > 0 && !this.counter.isLimit && this.counter.inQueue < settings.queueLimit;) operationMethod();
-      }, i * interval);
-    }
   }
 }
 
